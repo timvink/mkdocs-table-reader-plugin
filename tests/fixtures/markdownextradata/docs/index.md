5d2b5ba35249c810b3494f6663378930bbc8f7c1
Tim Vink <vinktim@gmail.com>
2020-06-24 20:30:44 +0200

    Add unit tests for markdownextradata

---
+++ tests/fixtures/markdownextradata/docs/index.md
@@ -0,0 +1,14 @@
+# Compatibility with markdownextradata
+
+https://github.com/rosscdh/mkdocs-markdownextradata-plugin
+
+## read_csv
+
+The latest numbers using `read_csv()`:
+
+{{ read_csv('assets/basic_table.csv') }}
+
+## extradata
+
+{{ customer.name }}
+<a href="{{ customer.web }}">{{ customer.web }}</a>
--- tests/test_build.py
+++ tests/test_build.py
@@ -51,8 +51,8 @@ def setup_clean_mkdocs_folder(mkdocs_yml_path, output_path):
         shutil.rmtree(testproject_path)
 
     # Copy correct mkdocs.yml file and our test 'docs/'
-    shutil.copytree("tests/basic_setup/docs", testproject_path / "docs")
-    shutil.copytree("tests/basic_setup/assets", testproject_path / "assets")
+    shutil.copytree(os.path.join(os.path.dirname(mkdocs_yml_path),"docs"), testproject_path / "docs")
+    shutil.copytree(os.path.join(os.path.dirname(mkdocs_yml_path),"assets"), testproject_path / "assets")
     shutil.copyfile(mkdocs_yml_path, testproject_path / "mkdocs.yml")
 
     return testproject_path
@@ -83,7 +83,7 @@ def build_docs_setup(testproject_path):
 
 def test_table_output(tmp_path):
 
-    tmp_proj = setup_clean_mkdocs_folder("tests/basic_setup/mkdocs.yml", tmp_path)
+    tmp_proj = setup_clean_mkdocs_folder("tests/fixtures/basic_setup/mkdocs.yml", tmp_path)
 
     result = build_docs_setup(tmp_proj)
     assert result.exit_code == 0, "'mkdocs build' command failed"
@@ -117,3 +117,22 @@ def test_table_output(tmp_path):
     contents = page_with_tag.read_text()
     assert re.search(r"table1", contents)
     assert re.search(r"table2", contents)
+
+
+def test_compatibility_markdownextradata(tmp_path):
+
+    tmp_proj = setup_clean_mkdocs_folder("tests/fixtures/markdownextradata/mkdocs.yml", tmp_path)
+
+    result = build_docs_setup(tmp_proj)
+    assert result.exit_code == 0, "'mkdocs build' command failed"
+
+    index_file = tmp_proj / "site/index.html"
+    assert index_file.exists(), f"{index_file} does not exist"
+
+    # Make sure with markdown tag has the output
+    page_with_tag = tmp_proj / "site/index.html"
+    contents = page_with_tag.read_text()
+    # Make sure the table is inserted
+    assert re.search(r"531456", contents)
+    # Make sure the extradata 'web' is inserted
+    assert re.search(r"www.example.com", contents)
