de0ecd39fd4a10c028e858fef59dff805af5c0ec
BartvanWoesik <bartwoesik1@gmail.com>
2023-06-01 22:12:44 +0200

    fix_kwargs_utils_functions

--- mkdocs_table_reader_plugin/utils.py
+++ mkdocs_table_reader_plugin/utils.py
@@ -5,10 +5,10 @@ def get_keywords(func):
     return [p.name for p in signature(func).parameters.values() if p.kind == p.POSITIONAL_OR_KEYWORD or p.kind == p.KEYWORD_ONLY]
 
 def kwargs_in_func(keywordargs, func):
-    return dict({(k,v) for k, v in keywordargs.items() if k in get_keywords(func)})
+    return {k:v for k, v in keywordargs.items() if k in get_keywords(func)}
 
 def kwargs_not_in_func(keywordargs, func):
-    return dict({(k,v) for k, v in keywordargs.items() if k not in get_keywords(func)})
+    return {k:v for k, v in keywordargs.items() if k not in get_keywords(func)}
 
 
 class cd:
